7b585e073cf8b9889339600b9f108822560ba63d
nicolake <nicolake@gmail.com>
2019-09-11 23:21:07 -0300

    Configuracion inicial de spring

---
+++ src/test/java/dev/nicolake/sistemaalumnos/SistemaAlumnosApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.nicolake.sistemaalumnos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaAlumnosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
